9fd7443d8cedc80e678856d6ca885c616ee8abee
kanori07 <judychen075@gmail.com>
2025-08-31 15:33:14 +0800

    更新teamwk連結

--- src/main/java/com/example/mygocar/controller/fronted/IndexController.java
+++ src/main/java/com/example/mygocar/controller/fronted/IndexController.java
@@ -30,4 +30,9 @@ public class IndexController {
     public String member() {
         return "fronted/member"; 
     }
+
+     @GetMapping("/teamwk")
+    public String teamwk() {
+        return "fronted/teamwk"; 
+    }
 }
