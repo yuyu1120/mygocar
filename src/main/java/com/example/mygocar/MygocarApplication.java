70f8bf0f20605fc693ad711b4c73541b5e359dc3
Chia Yu Kao <redkaoyu@gmail.com>
2025-09-06 16:01:51 +0800

    fix: Correct class name casing for MygocarApplication

--- src/main/java/com/example/mygocar/MygocarApplication.java
+++ src/main/java/com/example/mygocar/MygocarApplication.java
@@ -6,15 +6,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class mygocarApplication extends SpringBootServletInitializer {
+public class MygocarApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(mygocarApplication.class, args);
+		SpringApplication.run(MygocarApplication.class, args);
 	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(mygocarApplication.class);
+		return builder.sources(MygocarApplication.class);
 
 	}
 }
